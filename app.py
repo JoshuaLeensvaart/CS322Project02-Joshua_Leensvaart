3382855cf8749f23b43fbef4f2f043c923a097b2
JoshuaLeensvaart <josh.leensvaart@gmail.com>
2026-04-20 13:16:12 -0400

    added index

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+pangolin_logs = [
+    {"id": 1, "location": "Congo Basin", "weight": 4.5, "date": "2026-04-20"}
+]
 @app.route("/")
+def index():
+    # We pass the entire list to the frontend (Pattern from PDF Page 3)
+    return render_template('index.html', logs=pangolin_logs)
 def home():
     return '''
         <h1>Welcome to the edge of Pangolins</h1>
