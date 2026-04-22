9822957ed88baf22dba0805d3ed69f22879de458
JoshuaLeensvaart <josh.leensvaart@gmail.com>
2026-04-22 19:34:15 -0400

    small fixes

--- app.py
+++ app.py
@@ -23,11 +23,10 @@ def add_entry():
         flash("Date is required.")
         return redirect(url_for('index'))
     try:
-        # Convert string to date object for comparison
         input_date = datetime.datetime.strptime(dt, '%Y-%m-%d').date()
         today = datetime.date.today()
 
-        if (input_date > today):
+        if input_date > today:
             flash("Date Error: Cannot log a future sighting.")
             return redirect(url_for('index'))
     except ValueError:
